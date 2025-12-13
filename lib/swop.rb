919420a32f978af7b3eedf18f4a4a2a39dbb8690
Harissoooon <harissoooon@outlook.com>
2025-12-13 20:25:46 +0000

    Allow customer colour preference

--- lib/swop.rb
+++ lib/swop.rb
@@ -6,5 +6,23 @@ require "swop/railtie" if defined?(Rails)
 
 module Swop
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :theme_style
+    attr_accessor :theme_color
+
+    def initialize
+      @theme_style = :simple
+      @theme_color = :light
+    end
+  end
 end
