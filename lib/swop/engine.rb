aebdb1784e3ae65ddf6b1baeb92564652b263f3f
Harissoooon <harissoooon@outlook.com>
2025-12-29 14:59:29 +0000

    Make sure SCSS compiles on config change

--- lib/swop/engine.rb
+++ lib/swop/engine.rb
@@ -2,6 +2,11 @@ module Swop
   class Engine < ::Rails::Engine
     initializer "swop.assets.precompile" do |app|
       app.config.assets.precompile += %w[swop/favicon.ico]
+      app.config.assets.precompile += %w[swop/swop-themes/swop-simple/swop-simple-amethyst.css]
+      app.config.assets.precompile += %w[swop/swop-themes/swop-simple/swop-simple-dark.css]
+      app.config.assets.precompile += %w[swop/swop-themes/swop-simple/swop-simple-forest.css]
+      app.config.assets.precompile += %w[swop/swop-themes/swop-simple/swop-simple-light.css]
+      app.config.assets.precompile += %w[swop/swop-themes/swop-simple/swop-simple-royal.css]
     end
   end
 end
