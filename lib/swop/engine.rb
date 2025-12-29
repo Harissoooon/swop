d90056e4a3984d48e397e9641d65975eb891dd0c
Harissoooon <harissoooon@outlook.com>
2025-12-29 11:53:17 +0000

    Show favicon for theme

--- lib/swop/engine.rb
+++ lib/swop/engine.rb
@@ -1,5 +1,8 @@
 module Swop
   class Engine < ::Rails::Engine
     isolate_namespace Swop
+    initializer "swop.assets.precompile" do |app|
+      app.config.assets.precompile += %w[swop/favicon.ico]
+    end
   end
 end
